13ffcd4e365224e53e1214b4718dabf1ea4e8d25
zhe.li <zhe.li@ihandysoft.com>
2020-05-07 20:37:00 +0800

    Watermark - add webpack & babel for es6 support

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+
+var APP_PATH = path.join(__dirname, 'src');
+var BUILD_PATH = path.join(__dirname, '');
+
+module.exports = {
+    entry: {
+        bundle: [path.join(APP_PATH, 'main.js')]
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: 'index.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {importLoaders: 1}
+                    },
+                    'postcss-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                include: APP_PATH,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        modules: [APP_PATH, 'node_modules'],
+        extensions: ['.js']
+    },
+    mode: 'production',
+    performance: {
+        hints: false
+    }
+};
