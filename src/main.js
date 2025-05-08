56b444bdd57c2fcfd5f63d6cc744afca095bdaa6
Zhe <zhe.li@ewp-group.com>
2025-05-08 19:03:14 +0800

    Watermark - optimizing the code

--- src/main.js
+++ src/main.js
@@ -12,10 +12,9 @@ const removeNode = (id) => {
 };
 // Times33 hash
 const hashCode = (str) => {
-    let hash = 5381,
-        index = str.length;
-    while (index) {
-        hash = (hash * 33) ^ str.charCodeAt(--index);
+    let hash = 5381;
+    for (let i = 0; i < str.length; i++) {
+        hash = (hash << 5) + hash + str.charCodeAt(i); // hash * 33 + char
     }
     return hash >>> 0;
 };
@@ -27,46 +26,56 @@ const setWatermark = ({title = '', subtitle = '', width = 240, height = 180, ang
     let realId = invisible ? invisibleId : maskId;
     removeNode(realId);
 
-    let str1 = title;
-    let str2 = '';
-    let color = `rgba(128, 128, 128, ${alpha})`;
-    let zIndex = 10000;
-    if (invisible) {
-        str1 = hashShort(title);
-        str2 = hashShort(subtitle);
-        width = 120;
-        height = 120;
-        angle = 30;
-        fontSize = '22px';
-        color = 'rgba(128, 128, 128, 0.016)';
-        zIndex = 10001;
-    }
+    const config = invisible
+        ? {
+              str1: hashShort(title),
+              str2: hashShort(subtitle),
+              width: 120,
+              height: 120,
+              angle: 30,
+              fontSize: '22px',
+              color: 'rgba(128, 128, 128, 0.016)',
+              zIndex: 10001
+          }
+        : {
+              str1: title,
+              str2: subtitle || '',
+              width,
+              height,
+              angle,
+              fontSize,
+              color: `rgba(128, 128, 128, ${alpha})`,
+              zIndex: 10000
+          };
 
-    let stage = document.createElement('canvas');
-    stage.width = width;
-    stage.height = height;
+    const stage = document.createElement('canvas');
+    stage.width = config.width;
+    stage.height = config.height;
 
-    let ctx = stage.getContext('2d');
+    const ctx = stage.getContext('2d');
     ctx.translate(stage.width / 2, stage.height / 2);
-    ctx.rotate((angle * Math.PI) / 180);
-    ctx.font = `${fontSize} Arial`;
-    ctx.fillStyle = color;
+    ctx.rotate((config.angle * Math.PI) / 180);
+    ctx.font = `${config.fontSize} Arial`;
+    ctx.fillStyle = config.color;
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    ctx.fillText(str1, 0, -stage.height / 10);
-    ctx.fillText(str2, 0, stage.height / 10);
+    ctx.fillText(config.str1, 0, -stage.height / 10);
+    config.str2 && ctx.fillText(config.str2, 0, stage.height / 10);
 
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.id = realId;
-    div.style.pointerEvents = 'none';
-    div.style.top = '0px';
-    div.style.left = '0px';
-    div.style.position = 'fixed';
-    div.style.zIndex = zIndex;
-    div.style.width = '100%';
-    div.style.height = '100%';
-    div.style.opacity = 0.5;
-    div.style.background = 'url(' + stage.toDataURL('image/png') + ')';
+    Object.assign(div.style, {
+        pointerEvents: 'none',
+        top: '0px',
+        left: '0px',
+        position: 'fixed',
+        zIndex: config.zIndex,
+        width: '100%',
+        height: '100%',
+        opacity: 0.5,
+        background: `url(${stage.toDataURL('image/png')})`
+    });
+
     document.body.appendChild(div);
     return div;
 };
@@ -74,37 +83,31 @@ const setWatermark = ({title = '', subtitle = '', width = 240, height = 180, ang
 const watchNode = (elem, targetId, callback) => {
     let parent = elem.parentNode;
     if (!parent) {
-        return;
+        return {observer: null, watch: () => {}};
     }
     let observer = new MutationObserver((mutationsList) => {
         for (let mutation of mutationsList) {
-            switch (mutation.type) {
-                case 'childList':
-                    if (mutation.target === document.body) {
-                        for (const node of mutation.removedNodes) {
-                            if (node.id === targetId) {
-                                callback();
-                            }
-                        }
+            if (mutation.type === 'childList' && mutation.target === document.body) {
+                for (const node of mutation.removedNodes) {
+                    if (node.id === targetId) {
+                        callback();
+                        break;
                     }
-                    break;
-                case 'attributes':
-                    removeNode(mutation.target.id);
-                    callback();
-                    break;
-                default:
-                    break;
+                }
+            } else if (mutation.type === 'attributes') {
+                removeNode(mutation.target.id);
+                callback();
             }
         }
     });
     observer.observe(parent, {childList: true});
-    let watchSelf = (node) => {
+    const watchSelf = (node) => {
         observer.observe(node, {attributes: true, attributeOldValue: true});
     };
     watchSelf(elem);
 
     return {
-        observer: observer,
+        observer,
         watch: watchSelf
     };
 };
